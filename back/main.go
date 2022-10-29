5d476c12dab3ac4c65994a3942869d091b143075
Yuorei <yuorei71@gmail.com>
2022-10-29 23:16:09 +0900

    main.goにコメント追加

--- back/main.go
+++ back/main.go
@@ -9,22 +9,30 @@ import (
 )
 
 func main() {
-	router := gin.Default()
-	if err:=operateDb.Init();err!=nil{
+	// DBopen
+	if err := operateDb.Init(); err != nil {
 		panic("DBerror")
 	}
 
+	router := gin.Default()
+	// /users
 	router.POST("/users/", controller.GetUsers)
 	router.DELETE("/users", controller.DeleteUsers)
+
+	// /rent-lists
 	router.GET("/rent-lists", controller.GetAllRentLists) //実装あとで
 	router.GET("/rent-lists/:id", controller.GetSingleRentList)
 	router.POST("/rent-lists", controller.PostRentLists)
 	router.PUT("/rent-lists/:id", controller.PutRentLists)
 	router.DELETE("/rent-lists/:id", controller.DeleteRentLists)
+
+	// /lend-lists
 	router.GET("/lend-lists", controller.GetLendLists) //実装あとで
 	router.GET("/lend-lists/:id", controller.GetLendThing)
 	router.POST("/lend-lists", controller.PostLendLists)
 	router.PUT("/lend-lists/:id", controller.PutLendLists)
 	router.DELETE("/lend-lists/s:id", controller.DeleteLendList)
+
+	// sever
 	router.Run(":3000")
 }
