9a39d568fcd83e26d48da8787cd4a5fa00e04727
Yuorei <yuorei71@gmail.com>
2022-11-14 18:23:29 +0900

    print削除

--- back/firebaseOperation/firebase_init.go
+++ back/firebaseOperation/firebase_init.go
@@ -2,18 +2,15 @@ package firebaseOperation
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	firebase "firebase.google.com/go/v4"
 )
 
-func Init()*firebase.App {
+func Init() *firebase.App {
 	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
-	fmt.Println(app)
-	fmt.Println("jgirhgi")
 	return app
 }
